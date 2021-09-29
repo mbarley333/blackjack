a79dcea5d2b05a69dff6476c43c03ceac47f5943
mbarley333 <jacksonkato808@gmail.com>
2021-09-29 11:43:00 -1000

    update ai

--- blackjack/blackjack.go
+++ blackjack/blackjack.go
@@ -742,12 +742,22 @@ func AiActionBasic(output io.Writer, input io.Reader, player *Player, dealerCard
 		}
 	}
 
-	if handValue <= 11 {
+	if (handValue == 10 && dealerCardValue < handValue || handValue == 11 && dealerCardValue < handValue) && player.Cash > player.HandBet {
+		action = ActionDoubleDown
+	} else if handValue == 9 && dealerCardValue >= 3 && dealerCardValue <= 6 && player.Cash > player.HandBet {
+		action = ActionDoubleDown
+	} else if handValue <= 11 {
 		action = ActionHit
-	} else if handValue <= 17 && isSoft {
+	} else if handValue <= 15 && isSoft {
 		action = ActionHit
-	} else if handValue >= 18 && isSoft {
+	} else if handValue >= 19 && isSoft {
 		action = ActionStand
+	} else if handValue >= 16 && handValue <= 18 && isSoft && dealerCardValue >= 7 {
+		action = ActionHit
+	} else if handValue >= 16 && handValue <= 18 && isSoft && dealerCardValue <= 6 && player.Cash > player.HandBet {
+		action = ActionDoubleDown
+	} else if handValue >= 16 && handValue <= 18 && isSoft && dealerCardValue <= 6 && player.Cash < player.HandBet {
+		action = ActionHit
 	} else if handValue >= 17 && handValue <= 21 {
 		action = ActionStand
 	} else if handValue == 12 && dealerCardValue <= 3 {
--- blackjack/blackjack_test.go
+++ blackjack/blackjack_test.go
@@ -400,9 +400,43 @@ func TestAiBasicAction(t *testing.T) {
 		playerCards []cards.Card
 		dealerCard  cards.Card
 		action      blackjack.Action
+		bet         int
+		cash        int
 		description string
 	}
 	tcs := []testCase{
+		{
+			playerCards: []cards.Card{{Rank: cards.Seven, Suit: cards.Club}, {Rank: cards.Three, Suit: cards.Club}},
+			dealerCard:  cards.Card{Rank: cards.Four, Suit: cards.Club},
+			action:      blackjack.ActionDoubleDown,
+			bet:         1,
+			cash:        10,
+			description: "10 or 11, dealer < 10 or 11, enough to double",
+		},
+		{
+			playerCards: []cards.Card{{Rank: cards.Seven, Suit: cards.Club}, {Rank: cards.Three, Suit: cards.Club}},
+			dealerCard:  cards.Card{Rank: cards.Four, Suit: cards.Club},
+			action:      blackjack.ActionHit,
+			bet:         10,
+			cash:        1,
+			description: "10 or 11, dealer < 10 or 11, not enough to double",
+		},
+		{
+			playerCards: []cards.Card{{Rank: cards.Six, Suit: cards.Club}, {Rank: cards.Three, Suit: cards.Club}},
+			dealerCard:  cards.Card{Rank: cards.Four, Suit: cards.Club},
+			action:      blackjack.ActionDoubleDown,
+			bet:         1,
+			cash:        10,
+			description: "9, dealer between 3 and 6, enough to double",
+		},
+		{
+			playerCards: []cards.Card{{Rank: cards.Six, Suit: cards.Club}, {Rank: cards.Three, Suit: cards.Club}},
+			dealerCard:  cards.Card{Rank: cards.Four, Suit: cards.Club},
+			action:      blackjack.ActionHit,
+			bet:         10,
+			cash:        1,
+			description: "9, dealer between 3 and 6, not enough to double",
+		},
 		{
 			playerCards: []cards.Card{{Rank: cards.Ace, Suit: cards.Club}, {Rank: cards.Jack, Suit: cards.Club}},
 			dealerCard:  cards.Card{Rank: cards.Four, Suit: cards.Club},
@@ -422,10 +456,32 @@ func TestAiBasicAction(t *testing.T) {
 			description: "Soft 15 or less",
 		},
 		{
-			playerCards: []cards.Card{{Rank: cards.Ace, Suit: cards.Club}, {Rank: cards.Seven, Suit: cards.Club}},
+			playerCards: []cards.Card{{Rank: cards.Ace, Suit: cards.Club}, {Rank: cards.Eight, Suit: cards.Club}},
 			dealerCard:  cards.Card{Rank: cards.Four, Suit: cards.Club},
 			action:      blackjack.ActionStand,
-			description: "Soft 18 or higher",
+			description: "Soft 19 or higher",
+		},
+		{
+			playerCards: []cards.Card{{Rank: cards.Ace, Suit: cards.Club}, {Rank: cards.Six, Suit: cards.Club}},
+			dealerCard:  cards.Card{Rank: cards.Seven, Suit: cards.Club},
+			action:      blackjack.ActionHit,
+			description: "Soft 16 to 18, dealer >= 7",
+		},
+		{
+			playerCards: []cards.Card{{Rank: cards.Ace, Suit: cards.Club}, {Rank: cards.Six, Suit: cards.Club}},
+			dealerCard:  cards.Card{Rank: cards.Six, Suit: cards.Club},
+			action:      blackjack.ActionDoubleDown,
+			description: "Soft 16 to 18, dealer <= 6, enough to double",
+			bet:         1,
+			cash:        10,
+		},
+		{
+			playerCards: []cards.Card{{Rank: cards.Ace, Suit: cards.Club}, {Rank: cards.Six, Suit: cards.Club}},
+			dealerCard:  cards.Card{Rank: cards.Six, Suit: cards.Club},
+			action:      blackjack.ActionHit,
+			description: "Soft 16 to 18, dealer <= 6, not enough to double",
+			bet:         10,
+			cash:        1,
 		},
 		{
 			playerCards: []cards.Card{{Rank: cards.Ten, Suit: cards.Club}, {Rank: cards.Three, Suit: cards.Club}, {Rank: cards.Five, Suit: cards.Club}},
@@ -458,8 +514,10 @@ func TestAiBasicAction(t *testing.T) {
 
 	for _, tc := range tcs {
 		p := &blackjack.Player{
-			Hand:   tc.playerCards,
-			Decide: blackjack.AiActionBasic,
+			Hand:    tc.playerCards,
+			Decide:  blackjack.AiActionBasic,
+			HandBet: tc.bet,
+			Cash:    tc.cash,
 		}
 		g.AddPlayer(p)
 		g.Dealer.Hand = append(g.Dealer.Hand, tc.dealerCard)
